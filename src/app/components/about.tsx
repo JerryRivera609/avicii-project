1999884c083a0733e2fc90c2961b6712676f8215
JerryRivera609 <jerry609rivera@gmail.com>
2025-02-05 22:48:16 -0500

    Se agregaron y eliminaron algunas partes redundantes o que no iban con el diseño

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -48,18 +48,20 @@ useEffect(() => {
                         <div>
                         <h2 className="font-arizonia text-[8rem] leading-[7rem] mb-10">Remember Tim <br /> Berling</h2>
                         <div className="flex flex-col justify-center text-center">
-                            <p className="font-sans mt-5 text-[1.5rem]" >- LIVE A LIFE YOU WILL REMEMBER -</p>
+                            <p className="font-sans mt-5 text-[1.5rem]" >LIVE A LIFE YOU WILL REMEMBER</p>
                             <p className="text-[1.2rem] tracking-[0.5rem]"> 1989 - 2018</p>
+                            <p>◢◤</p>
                         </div>
                     </div>
                 </section>
-                <section className="min-w-[80vw] h-[95vh] p-10 bg-[#222831] rounded-[25px] flex">
-                    <div className="space-y-5">
+                <section className="min-w-[80vw] h-[95vh] p-10 bg-[#222831] rounded-[25px] flex gap-4">
+                    <div className="space-y-5 w-1/3 flex flex-col justify-center">
                         <img src={timeline02} className="w-[300px] h-auto rounded-xl" alt="" />
                         <img src={timeline01} className="w-[300px] h-auto rounded-xl" alt="" />
                     </div>
-                    <div>
-                        <h2>Tim Berling - 1989</h2>
+                    <div className="flex flex-col text-[1.2rem] w-2/3 justify-center items-center text-center">
+                        <h2 className="font-lato text-[3rem]">TIM BERLING - 1989</h2>
+                        <p>Tim Bergling was born on September 8, 1989, in Stockholm, Sweden. From a young age, he showed a strong passion for music and creativity. Inspired by artists like Daft Punk and Swedish House Mafia, he began experimenting with music production during his teenage years.</p>
                     </div>
                 </section>
                 <section className="min-w-[80vw] h-[95vh] p-5">🔥 2011 - "Levels" lo hace famoso</section>
