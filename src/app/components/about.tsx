08c8f38a2f1dbea723180038472b8acbed5da74d
JerryRivera609 <jerry609rivera@gmail.com>
2025-02-05 21:43:43 -0500

    Se corrigieron estilos en el time line

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -4,9 +4,8 @@ import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./about.css";
-const timeline = '/timeline.png'
-const timelinea01 = '/timeline01.jpg'
 
+const timeline = './timeline.png'
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -19,12 +18,13 @@ useEffect(() => {
     const sections = sectionsRef.current;
 
     if (container && sections) {
+      // Reseteamos el scroll al principio
         window.scrollTo({ top: 0, behavior: "smooth" });
 
         gsap.to(sections, {
-        x: () => -1 * (sections.scrollWidth - window.innerWidth),
-        ease: "none",
-        scrollTrigger: {
+            x: () => -1 * (sections.scrollWidth - window.innerWidth),
+            ease: "none",
+            scrollTrigger: {
             trigger: container,
             start: "top top",
             end: () => `+=${sections.scrollWidth}`,
@@ -39,30 +39,22 @@ useEffect(() => {
     return (
         <div className="timeline-container" ref={containerRef}>
             <div className="timeline-sections" ref={sectionsRef}>
-            <section className="timeline-item flex flex-col"
-            style={{ backgroundImage: `url(${timeline})` }}
-            >
-                <div>
-                    <h2 className="font-arizonia text-[8rem] leading-[7rem] mb-10">Remember Tim <br /> Berling</h2>
-                    <div className="flex flex-col justify-center text-center">
-                        <p className="font-sans mt-5" >- LIVE A LIFE YOU WILL REMEMBER -</p>
-                        <p className="text-[1rem] tracking-[0.5rem]"> 1989 - 2018</p>
+                <section className="min-w-[80vw] h-[100vh] p-5 bg-[#05051b] rounded-xl flex justify-around items-center">
+                    <div>
+                        <img src={timeline} alt="" />
                     </div>
-                </div>
-            </section>
-                <section className="timeline-item flex flex-col"
-                style={{ backgroundImage: `url(${timelinea01})` }}
-                >
-                    <div className="justify-end items-start">
-                        <h2 className="font-lato text-[3rem]">TIM BERLING</h2>
-                        <p>08 de septiembre de 1989</p>
-                        <p>Estocolmo - Suecia</p>
-                        <p>Desde joven, mostró una fuerte pasión por la música y la creatividad. Inspirado por artistas como Daft Punk y Swedish House Mafia, comenzó a experimentar con la producción musical desde su adolescencia.</p>
+                        <div>
+                        <h2 className="font-arizonia text-[8rem] leading-[7rem] mb-10">Remember Tim <br /> Berling</h2>
+                        <div className="flex flex-col justify-center text-center">
+                            <p className="font-sans mt-5" >- LIVE A LIFE YOU WILL REMEMBER -</p>
+                            <p className="text-[1rem] tracking-[0.5rem]"> 1989 - 2018</p>
+                        </div>
                     </div>
                 </section>
-                <section className="timeline-item">🔥 2011 - "Levels" lo hace famoso</section>
-                <section className="timeline-item">🎤 2013 - "Wake Me Up" rompe récords</section>
-                <section className="timeline-item">💿 2019 - Álbum póstumo "TIM"</section>
+                <section className="min-w-[80vw] h-[100vh] p-5">🎧 1989 - Nacimiento en Estocolmo</section>
+                <section className="min-w-[80vw] h-[100vh] p-5">🔥 2011 - "Levels" lo hace famoso</section>
+                <section className="min-w-[80vw] h-[100vh] p-5">🎤 2013 - "Wake Me Up" rompe récords</section>
+                <section className="min-w-[80vw] h-[100vh] p-5">💿 2019 - Álbum póstumo "TIM"</section>
             </div>
         </div>
     );
